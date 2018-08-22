c425cb13bc44ab8a20116c1430266d9d871fc1ee
stonkeep <stonkeep@users.noreply.github.com>
2018-08-22 14:19:52 -0300

    teste pedidos crud simples

--- tests/Unit/PedidosTest.php
+++ tests/Unit/PedidosTest.php
@@ -39,9 +39,11 @@ class PedidosTest extends TestCase
     public function testExample()
     {
         //cria cliente
-        $cliente = factory(Clientes::class)->create();
+        factory(Clientes::class, 3)->create();
+        $cliente = Clientes::firstOrFail();
         //cria ciclista
-        $ciclista = factory(Ciclista::class)->create();
+        factory(Ciclista::class, 3)->create();
+        $ciclista = Ciclista::firstOrFail();
         //cria pedido
         $pedido = Pedidos::create();
         //vincula pedido a cliente
@@ -53,7 +55,34 @@ class PedidosTest extends TestCase
         //Verficia se o pedido tem o mesmo ciclista
         $this->assertEquals($ciclista->id,Pedidos::firstOrFail()->ciclista->id);
 
+        //Altera o ciclista do pedido
+        Pedidos::firstOrFail()->ciclista()->associate(Ciclista::find(2))->save();
+        //Verifica se o ciclista do pedido foi alterado
+        $this->assertEquals(Pedidos::firstOrFail()->ciclista->id, Ciclista::find(2)->id);
 
+        //deleta pedido
+        Clientes::firstOrFail()->pedidos()->find(1)->delete();
+        //verifica se o pedido foi deletado
+        $this->assertEmpty(Clientes::firstOrFail()->pedidos()->find(1));
+    }
+
+    //TODO crud com rotas
+
+    /** @test */
+    public function crud_de_rotas()
+    {
+        //cria cliente
+        factory(Clientes::class, 3)->create();
+        $cliente = Clientes::firstOrFail();
+        //cria ciclista
+        factory(Ciclista::class, 3)->create();
+        //testa validacao do pedido
+        $this->post(route('pedidos.store'), []);
+        //cadastra pedido
+        //le pedido
+        //altera pedido
+        //deleta pedido
+        //verifica se o softdelete esta funcionando
 
     }
 
